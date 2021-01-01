7a6fbcb1ec0af5187ba52c6a64c833534fc5b981
Peter Park 2.0 <peterparkk25+@gmail.com>
2021-01-01 23:08:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -62,19 +62,19 @@ def monopoly_with_fourth_tier
       :names => {
         :reading_railroad => 
         {
-          :mortgage_value => "100"
+          :mortgage_value => 100
         },
         :pennsylvania_railroad => 
         {
-          :mortage_value => "200"
+          :mortage_value => 200
         },
         :b_and_o_railroad => 
         {
-          :mortgage_value => "400"
+          :mortgage_value => 400
         },
         :shortline_railroad => 
         {
-          :mortgage_value => "800"
+          :mortgage_value => 800
         }
       },
       }
